385c728e7afbbd474f5d7899a4be14d5dcd3917b
abramov_am <abramov_am@rambler.ru>
2023-09-02 17:59:31 +0300

    sound

--- src/ParticleCanvas.js
+++ src/ParticleCanvas.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useRef } from 'react';
-
+import React, { useEffect,useRef } from 'react';
+import explosionSound from './sounds/explosion.mp3';
 const ParticleCanvas = () => {
   const canvasRef = useRef(null);
   const particles = useRef([]);
-
+  const mousePos = useRef({ x: 0, y: 0 });
+  
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
@@ -21,7 +22,9 @@ const ParticleCanvas = () => {
           y: 0,
           speed: 1 + Math.random(),
           radius: 7 + Math.random() * 7,
-          color: "#e8dfdb"
+          color: '#e8dfdb',
+          isExploding: false,
+          explosionTick: 0,
         };
         particles.current.push(newParticle);
       }
@@ -29,12 +32,29 @@ const ParticleCanvas = () => {
 
     const updateParticles = () => {
       particles.current.forEach((part) => {
+        if (part.isExploding) {
+          part.explosionTick++;
+          if (part.explosionTick > 10) {
+            const explosionAudio = new Audio(explosionSound);
+            explosionAudio.play();
+            particles.current.splice(particles.current.indexOf(part), 1);
+          }
+          return;
+        }
+
         part.y += part.speed;
+
+        const distance = Math.sqrt(
+          (part.x - mousePos.current.x) ** 2 + (part.y - mousePos.current.y) ** 2
+        );
+        if (distance < 50) {
+          part.isExploding = true;
+        }
       });
     };
 
     const drawParticles = () => {
-      context.fillStyle = "#3b2920";
+      context.fillStyle = '#3b2920';
       context.fillRect(0, 0, canvas.width, canvas.height);
 
       particles.current.forEach((part) => {
@@ -54,6 +74,10 @@ const ParticleCanvas = () => {
       });
     };
 
+    const handleMouseClick = (event) => {
+      mousePos.current = { x: event.clientX, y: event.clientY };
+    };
+
     const loop = () => {
       requestAnimationFrame(loop);
       createParticles();
@@ -64,11 +88,13 @@ const ParticleCanvas = () => {
 
     resizeCanvas();
     window.addEventListener('resize', resizeCanvas);
+    canvas.addEventListener('click', handleMouseClick);
 
     loop();
 
     return () => {
       window.removeEventListener('resize', resizeCanvas);
+      canvas.removeEventListener('click', handleMouseClick);
       cancelAnimationFrame(loop);
     };
   }, []);
